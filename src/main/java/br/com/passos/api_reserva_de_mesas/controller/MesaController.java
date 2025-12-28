cd8aac1d9bdfb6e6a0c17e6669e6029e969d4d9f
paulopassos88 <paulo.passos.pereira1@outlook.com>
2025-12-28 08:29:58 -0300

    Adicione MesaController e MesaService para registro de tabelas

---
+++ src/main/java/br/com/passos/api_reserva_de_mesas/controller/MesaController.java
@@ -0,0 +1,27 @@
+package br.com.passos.api_reserva_de_mesas.controller;
+
+import br.com.passos.api_reserva_de_mesas.domain.mesa.Mesa;
+import br.com.passos.api_reserva_de_mesas.service.MesaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mesas")
+public class MesaController {
+
+    private final MesaService mesaService;
+
+    public MesaController(MesaService mesaService) {
+        this.mesaService = mesaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Mesa> cadastrar(@RequestBody Mesa mesa) {
+        Mesa mesaSalva = mesaService.cadastrar(mesa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(mesaSalva);
+    }
+}
---
+++ src/main/java/br/com/passos/api_reserva_de_mesas/service/MesaService.java
@@ -0,0 +1,21 @@
+package br.com.passos.api_reserva_de_mesas.service;
+
+import br.com.passos.api_reserva_de_mesas.domain.mesa.Mesa;
+import br.com.passos.api_reserva_de_mesas.domain.mesa.MesaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class MesaService {
+
+    private final MesaRepository mesaRepository;
+
+    public MesaService(MesaRepository mesaRepository) {
+        this.mesaRepository = mesaRepository;
+    }
+
+    @Transactional
+    public Mesa cadastrar(Mesa mesa) {
+        return mesaRepository.save(mesa);
+    }
+}
